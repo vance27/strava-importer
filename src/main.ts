1c6dd25394311e5d18241a0852b78394db18fc4b
Brian Vance <briakvanc27@gmail.com>
2025-01-23 15:09:32 -0600

    random code

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,37 @@
-import { AthleteResponse } from "strava-v3";
+import { AthleteResponse, default as strava } from "strava-v3";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 dotenv.config({ path: [".env.local"], override: true });
-// console.log(process.env);
-// strava.config({
-//   access_token: "",
-//   client_id: "",
-//   client_secret: "",
-//   redirect_uri: "",
-// });
 
-// const payload = await strava.athlete.get({});
-// console.log(payload);
 const BASE_URL = "https://www.strava.com/api/v3";
 async function main() {
   try {
-    // const url: string = await strava.oauth.getRequestAccessURL({
-    //   scope: "view_private,write",
+    const refresh = await strava.oauth.refreshToken(
+      process.env.STRAVA_REFRESH_TOKEN
+    );
+    console.log(refresh);
+    // const getToken: { access_token: string } = await strava.oauth.getToken(
+    //   process.env.STRAVA_REFRESH_TOKEN
+    // );
+    const accessToken = refresh.access_token;
+    const api: typeof strava = new strava.client(accessToken);
+    // const res = await fetch(`${BASE_URL}/athlete`, {
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: `Bearer ${accessToken}`,
+    //   },
     // });
-    // console.log(url);
-    // const res = await strava.oauth.getToken(url);
-    // console.log(res);
-    const res = await fetch(`${BASE_URL}/athlete`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${process.env.STRAVA_ACCESS_TOKEN}`,
-      },
-    });
-    if (res.ok) {
-      const body: AthleteResponse = await res.json();
-      console.log(body);
-    }
+    // if (res.ok) {
+    //   const body: AthleteResponse = await res.json();
+    //   console.log(body);
+    // }
+
+    const athlete = await api.athlete.get({});
+    console.log(athlete);
+    const lastWeekActivities = await api.athlete.listActivities({});
+    // const activities = await api.activities.get({});
+    // console.log(activities);
   } catch (e) {
     console.error("Error occurred", e.message);
   }
